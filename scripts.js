0bc6245a152c55e0dea51d023715c9b452ba8bd4
tyler <tylerjyoungdahl@gmail.com>
2024-03-02 18:27:52 -0800

    set up ui and DOM variables

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const displayTop = document.querySelector("#display-top");
+const displayBottom = document.querySelector("#display-bottom");
+
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+const equals = document.querySelector("#equals");
+const point = document.querySelector("#point");
+const backspace = document.querySelector("#backspace");
+const clear = document.querySelector("#clear");
